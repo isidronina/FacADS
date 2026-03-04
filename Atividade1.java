e6b4cb1a3990d8f51e733994fdde5c08a38ec6f1
isidronina <isidrosaavedra123456789@gmail.com>
2026-03-04 06:18:37 -0300

    Add files via upload

---
+++ Atividade1.java
@@ -0,0 +1,30 @@
+//Ariel Isidro Nina Saavedra
+
+class Ap {
+    public static void main (String [] args){
+
+        //Exercício 1
+        
+        System.out.println("Meu noem é: Ariel Isidro Nina Saavedra");
+        System.out.println("Meu registro é:" +111111111 );
+        System.out.println("Eu estou cursando: Tecnico em Analise e Desenvolvimento de Sistemas");
+        
+        //Exercício 2
+
+        int Idade=18;
+        double Altura=1.69;
+        String Status="Presente"; 
+
+        System.out.println("Minha idade é: " +Idade);
+        System.out.println("Minha altura é: " +Altura);
+        System.out.println("Eu estava " +Status+ " no dia da aula");
+
+
+        //Execício 3 printf 1
+
+        System.out.println("\nTabuada do 5:");
+        int numero=5;
+        for (int i=1 ; i <=10; i++){System.out.printf("%d X %d = %d%n",numero,i,numero*i);}
+
+    }
+}

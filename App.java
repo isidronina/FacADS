0b7d6a6518b6e231129eabd1e1b5210535123347
isidronina <isidrosaavedra123456789@gmail.com>
2026-03-10 14:43:37 -0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,24 +1,16 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner e=new Scanner(System.in);
+      
+        System.out.println("Digite um numero maior que 20");
+        double n1= e.nextDouble();
 
-        Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite o nome do(a) Estudante: ");
-        String nome=entrada.nextLine();
-
-        System.out.println("Digite a primeira nota: ");
-        double nota1 = entrada.nextDouble();
+        if(n1>20){
+            System.out.println("a metade do numero digitado é: "+n1/2);
+        }
         
-        System.out.println("Digite a segunada nota: ");
-        double nota2 = entrada.nextDouble();
-        
-        double media= (nota1+nota2)/2;
-
-        System.out.println("Seu nome é " +nome+ " e sua media final é : " +media);
-
-        entrada.close();
-
-    } //fecha o metodo principal
-}//facha o Scanner
+    }
+}
